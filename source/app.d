f7f6364936eef188593a342ffa57bc08019a9fb7
Harrison Wine <harrisonmwine@protonmail.com>
2025-02-04 15:14:33 -0500

    handle add instruction

--- source/app.d
+++ source/app.d
@@ -1,52 +1,70 @@
 import std.stdio;
+import std.stdlib;
 
-void main()
+immutable maxMemory = 1 << 16;
+immutable pcStart = 0x3000;
+enum Registers 
 {
-	immutable maxMemory = 1 << 16;
-	immutable pcStart = 0x3000;
-	enum Registers 
-	{
-		R0, 
-		R1, 
-		R2, 
-		R3, 
-		R4, 
-		R5, 
-		R6, 
-		R7, 
-		PC,
-		COND,
-		COUNT,
-	}
-	enum Opcodes 
-	{
-		BR,     /* branch */
-		ADD,    /* add  */
-		LD,     /* load */
-		ST,     /* store */
-		JSR,    /* jump register */
-		AND,    /* bitwise and */
-		LDR,    /* load register */
-		STR,    /* store register */
-		RTI,    /* unused */
-		NOT,    /* bitwise not */
-		LDI,    /* load indirect */
-		STI,    /* store indirect */
-		JMP,    /* jump */
-		RES,    /* reserved (unused) */
-		LEA,    /* load effective address */
-		TRAP,   /* execute trap */
-	}
-	enum ConditionFlags
-	{	
-		POS = 1 << 0, /* P */
-		ZRO = 1 << 1, /* Z */
-		NEG = 1 << 2, /* N */
-	}
-	ushort[maxMemory] memory;
-	ushort[Registers.COUNT] reg;
+	R0, 
+	R1, 
+	R2, 
+	R3, 
+	R4, 
+	R5, 
+	R6, 
+	R7, 
+	PC,
+	COND,
+	COUNT,
+}
+enum Opcodes 
+{
+	BR,     /* branch */
+	ADD,    /* add  */
+	LD,     /* load */
+	ST,     /* store */
+	JSR,    /* jump register */
+	AND,    /* bitwise and */
+	LDR,    /* load register */
+	STR,    /* store register */
+	RTI,    /* unused */
+	NOT,    /* bitwise not */
+	LDI,    /* load indirect */
+	STI,    /* store indirect */
+	JMP,    /* jump */
+	RES,    /* reserved (unused) */
+	LEA,    /* load effective address */
+	TRAP,   /* execute trap */
+}
+enum ConditionFlags
+{	
+	POS = 1 << 0, /* P */
+	ZRO = 1 << 1, /* Z */
+	NEG = 1 << 2, /* N */
+}
+ushort[maxMemory] memory;
+ushort[Registers.COUNT] reg;
+
+void main(string[] args)
+{
+	args = args[1 .. $];
+
 
 	//load arguments
+	if (args.length < 1)
+	{
+		writeln("dlc3 [image-file1] ...");
+	}
+
+	foreach (image; args)
+	{
+		if (!read_image(image))
+		{
+			writeln("ERROR: failed to load image: ", image);
+			return 1;
+		}
+	}
+
 	//setup
 
 	//Only one condition flag can be set at a given time; initially, set it to the Z flag.
@@ -65,7 +83,7 @@ void main()
 		switch (opcode)
 		{
 			case Opcodes.ADD:
-
+				add_instruction(instruction);
 				break;
 			case Opcodes.AND:
 
@@ -114,4 +132,58 @@ void main()
 	}
 	//Shutdown
 	writeln("Edit source/app.d to start your project.");
+}
+/** 
+ * 
+ * Params:
+ *   x = binary val to extend
+ *   bit_count = number of bits to extend by
+ */
+ushort extend_sign(ushort x, int bit_count)
+{
+	return (x >> (bit_count - 1) & 1) ? (x | (0xFFFF << bit_count)) : x;
+}
+
+/** 
+ * Any time a value is written to a register the flags need to be updated to indicate sign.
+ * Params:
+ *   r = register
+ */
+void update_flags(ushort r)
+{
+	if (reg[r] == 0)
+	{
+		reg[Registers.COND] = ConditionFlags.ZRO;
+	}
+	else if (reg[r] >> 15) //negative value
+	{
+		reg[Registers.COND] = ConditionFlags.NEG;
+	}
+	else
+	{
+		reg[Registers.COND] = ConditionFlags.POS;
+	}
+}
+
+void add_instruction(ushort instruction)
+{
+	/// destination register (DR)
+	ushort r0 = (instruction >> 9) & 0x1;
+	/// first operand (SR1)
+	ushort r1 = (instruction >> 6) & 0x7;
+	/// immediate mode flag
+	ushort imm_flag = ( instruction >> 5) & 0x1;
+	
+	if (imm_flag)
+	{
+		ushort imm5 = sign_extend(instruction & 0x1F, 5);
+		reg[r0] = reg[r1] + imm5;
+	}
+	else 
+	{
+		ushort r2 = instruction & 0x7;
+		reg[r0] = reg[r1] + reg[r2];	
+	}
+
+	update_flags(r0);
 }
